8280e8d83b8c2414ff6d74f8fdfefcbb48b75010
Alan Shaw <alan.shaw@protocol.ai>
2019-04-08 16:13:20 +0100

    feat: compatibility with go-libp2p-mdns
    This PR adds a compatibility class that allows a js-libp2p node to find a go-libp2p node (and vice versa) over MDNS.
    
    It's implemented as a separate class so the two differing implementations do not get confused.
    
    I've verified this is working correctly by running a go-ipfs and js-ipfs node with no boostrap nodes (and no other discovery methods) and verifying they find each other.
    
    TODO:
    
    * [ ] Add tests!
    
    Some tips if you want to try this out:
    
    * After you've run `ipfs init`, remember to remove the bootstrap nodes from the config file (`~/.ipfs/config`) of each node before you start up
    * Use `ipfs log level mdns debug` for some go-ipfs mdns logs
    * You can use the following script (after `npm link`ing this branch) to start a js-ipfs node with no bootstrap nodes and no discovery modules other than MDNS:
    
    ```js
    const IPFS = require('ipfs')
    const MDNS = require('libp2p-mdns')
    const TCP = require('libp2p-tcp')
    
    const ipfs = new IPFS({
      repo: '/tmp/ipfs-mdns',
      config: {
        Bootstrap: []
      },
      libp2p: {
        modules: {
          peerDiscovery: [MDNS],
          transport: [TCP]
        }
      }
    })
    
    ipfs.on('ready', async () => {
      console.log('ipfs is ready')
      console.log('My Peer ID:', (await ipfs.id()).id)
      setInterval(async () => {
        const peers = await ipfs.swarm.peers()
        console.log(peers.length, 'peers:')
        peers.forEach(p => console.log(p.peer.toB58String()))
      }, 10000)
    })
    ```
    
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

---
+++ src/compat/querier.js
@@ -0,0 +1,144 @@
+'use strict'
+
+const assert = require('assert')
+const EE = require('events')
+const MDNS = require('multicast-dns')
+const Multiaddr = require('multiaddr')
+const PeerInfo = require('peer-info')
+const PeerId = require('peer-id')
+const log = require('debug')('libp2p:mdns:compat:querier')
+const { SERVICE_TAG_LOCAL, MULTICAST_IP, MULTICAST_PORT } = require('./constants')
+
+class Querier extends EE {
+  constructor (peerId, options) {
+    super()
+    assert(peerId, 'missing peerId parameter')
+    this._peerIdStr = peerId.toB58String()
+    this._options = options || {}
+    this._options.queryPeriod = this._options.queryPeriod || 5000
+    this._onResponse = this._onResponse.bind(this)
+  }
+
+  start (callback) {
+    this._handle = periodically(() => {
+      // Create a querier that queries multicast but gets responses unicast
+      const querier = MDNS({ multicast: false, interface: '0.0.0.0', port: 0 })
+
+      querier.on('response', this._onResponse)
+
+      querier.query({
+        id: nextId(), // id > 0 for unicast response
+        questions: [{ name: SERVICE_TAG_LOCAL, type: 'PTR', class: 'IN' }]
+      }, null, {
+        address: MULTICAST_IP,
+        port: MULTICAST_PORT
+      })
+
+      return { stop: callback => querier.destroy(callback) }
+    }, this._options.queryPeriod)
+
+    setImmediate(() => callback())
+  }
+
+  _onResponse (event, info) {
+    const answers = event.answers || []
+    const ptrRecord = answers.find(a => a.type === 'PTR' && a.name === SERVICE_TAG_LOCAL)
+
+    // Only deal with responses for our service tag
+    if (!ptrRecord) return
+
+    log('got response', event, info)
+
+    const txtRecord = answers.find(a => a.type === 'TXT')
+    if (!txtRecord) return log('missing TXT record in response')
+
+    let peerIdStr
+    try {
+      peerIdStr = txtRecord.data[0].toString()
+    } catch (err) {
+      return log('failed to extract peer ID from TXT record data', txtRecord, err)
+    }
+
+    if (this._peerIdStr === peerIdStr) return // replied to myself, ignore
+
+    let peerId
+    try {
+      peerId = PeerId.createFromB58String(peerIdStr)
+    } catch (err) {
+      return log('failed to create peer ID from TXT record data', peerIdStr, err)
+    }
+
+    PeerInfo.create(peerId, (err, info) => {
+      if (err) return log('failed to create peer info from peer ID', peerId, err)
+
+      const srvRecord = answers.find(a => a.type === 'SRV')
+      if (!srvRecord) return log('missing SRV record in response')
+
+      log('peer found', peerIdStr)
+
+      const { port } = srvRecord.data || {}
+      const protos = { A: 'ip4', AAAA: 'ip6' }
+
+      const multiaddrs = answers
+        .filter(a => ['A', 'AAAA'].includes(a.type))
+        .reduce((addrs, a) => {
+          const maStr = `/${protos[a.type]}/${a.data}/tcp/${port}`
+          try {
+            addrs.push(new Multiaddr(maStr))
+            log(maStr)
+          } catch (err) {
+            log(`failed to create multiaddr from ${a.type} record data`, maStr, port, err)
+          }
+          return addrs
+        }, [])
+
+      multiaddrs.forEach(addr => info.multiaddrs.add(addr))
+      this.emit('peer', info)
+    })
+  }
+
+  stop (callback) {
+    this._handle.stop(callback)
+  }
+}
+
+module.exports = Querier
+
+function periodically (run, period) {
+  let handle, timeoutId
+  let stopped = false
+
+  const reRun = () => {
+    handle = run()
+    timeoutId = setTimeout(() => {
+      handle.stop(err => {
+        if (err) log(err)
+        if (!stopped) reRun()
+      })
+      handle = null
+    }, period)
+  }
+
+  reRun()
+
+  return {
+    stop (callback) {
+      stopped = true
+      clearTimeout(timeoutId)
+      if (handle) {
+        handle.stop(callback)
+      } else {
+        callback()
+      }
+    }
+  }
+}
+
+const nextId = (() => {
+  let id = 1
+  return () => {
+    id++
+    if (id === Number.MAX_SAFE_INTEGER) id = 1
+    return id
+  }
+})()
